a7417138ec1e9075a4b8d027e835aa22b78f0c47
lg <308584349@163.com>
2020-11-18 15:07:21 +0800

    add socket_test containing socket_server_client code

---
+++ socket_test/net_test/wrap.h
@@ -0,0 +1,21 @@
+/*************************************************************************
+	> File Name: wrap.h
+	> Author: liugang
+	> Mail: 308584349@163.com 
+	> Created Time: Sun 27 Sep 2020 10:14:27 AM CST
+ ************************************************************************/
+
+#include <stdio.h>
+
+void perr_exit(const char *s);
+int Accept(int fd, struct sockaddr *sa, socklen_t *salenptr);
+void Bind(int fd, const struct sockaddr *sa, socklen_t salen);
+void Connect(int fd, const struct sockaddr *sa, socklen_t salen);
+void Listen(int fd, int backlog);
+int Socket(int family, int type, int protocol);
+ssize_t Read(int fd, void *ptr, size_t nbytes);
+ssize_t Write(int fd, const void *ptr, size_t nbytes);
+void Close(int fd);
+ssize_t Readn(int fd, void *vptr, size_t n);
+ssize_t Writen(int fd, const void *vptr, size_t n);
+
